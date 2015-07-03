ce9a53c2e0a4be82c3a58e61fc2b08dbe685fb2d
Jim Paulo Ovejera <f360.jovejera@gmail.com>
2015-07-03 10:43:52 +0800

    Added 'set as default' checkbox

--- src/com/guevent/gibx/jim/Main.java
+++ src/com/guevent/gibx/jim/Main.java
@@ -1,10 +1,18 @@
 package com.guevent.gibx.jim;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
 import java.lang.reflect.InvocationTargetException;
 
+import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
@@ -45,11 +53,71 @@ public class Main {
 	}
 	
 	public static void main(String args[]) throws Exception{
-		// "\\\\192.168.1.44/Documents/JIM/GED.mdb"
-		// "C:/GED/GED.mdb"
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		Object db = JOptionPane.showInputDialog(null, "GIBX Database: ", "J:\\GED.mdb");
-		new Main((String)db);
+		
+		JTextField txtUrl = new JTextField();
+		String DEFAULT = getDefault(txtUrl);
+		txtUrl.setText(DEFAULT);
+		JCheckBox checkbox = new JCheckBox("Set as default path", true);
+		
+		String message = "Enter Admin Database:";
+		Object[] params = {message, txtUrl, checkbox};
+		String[] options = new String[2];
+		options[0] = new String("Connect");
+		options[1] = new String("Cancel");
+		int res = JOptionPane.showOptionDialog(null, params ,"GIBX Connection v.2.0", 0,JOptionPane.INFORMATION_MESSAGE,null,options,null);	
+		
+		System.out.println(res);
+		if(res == 1 || res == -1) System.exit(0);
+		
+		if(checkbox.isSelected()){
+			writeNewDefault(txtUrl.getText());
+		}
+		
+		new Main(txtUrl.getText());
+	}
+	
+	public static void writeNewDefault(String path){
+		try{
+			File ini = new File(System.getenv("APPDATA") + "\\GIBX\\gibx.ini");
+			PrintWriter writer = new PrintWriter(ini);
+			writer.write("path=" + path);
+			writer.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static String getDefault(JTextField txtUrl){
+		//GIBX ini directory from AppData
+		File mainDir = new File(System.getenv("APPDATA") + "\\GIBX");
+		
+		//GIBX ini file
+		File ini = new File(mainDir.getAbsolutePath() + "\\gibx.ini");
+		
+		//Create dir if dir not exist
+		if(!mainDir.exists()) mainDir.mkdir();
+		
+		//Create file if gibx.ini not exist
+		if(!ini.exists()){
+			try {
+				PrintWriter writer = new PrintWriter(ini);
+				writer.write("path=" + "\\\\Oman-PC\\Users\\Public\\GED.mdb");
+				writer.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		//read gibx.ini and return whatever string except null
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(ini));
+			String path = reader.readLine();
+			if(path != null) return path.split("=")[1];
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "";
 	}
 
 }
